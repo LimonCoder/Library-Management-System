c10f65eae5cfcd02926bf797f7464b0ac32b94db
Nurul Amin Limon <nurulaminlimon261893@gmail.com>
2020-04-11 21:46:12 +0600

    Add admin option added

--- liberian/admin.php
+++ liberian/admin.php
@@ -6,7 +6,7 @@ require_once('include/header.php') ?>
     <div class="col-sm-12">
         <h4 class="section-subtitle"><b>Book List :</b></h4>
         <div class="row text-right" style="margin-right: 6px">
-            <button class="btn btn-primary">Add Admin</button>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" >Add Admin</button>
         </div>
         <div class="panel">
             <div class="panel-content">
@@ -43,8 +43,9 @@ require_once('include/header.php') ?>
                                     <td><?= $row['Number'] ?></td>
                                     <td><?= $row['Address'] ?></td>
                                     <td>
-                                        <button class="btn btn-danger"><i class="fa fa-trash-o"></i></button>
-                                        <a href="" class="btn btn-primary"><i class="fa fa-"></i></a>
+                                        <a href="editprofile.php" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
+                                        <button class="btn btn-danger" style="margin-top: -10px;"><i class="fa fa-trash-o"></i></button>
+
                                     </td>
                                 </tr>
 
@@ -55,6 +56,49 @@ require_once('include/header.php') ?>
 
                         </tbody>
                     </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title" id="exampleModalLabel">Add User</h3>
+                    
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Name :</label>
+                            <input type="text" class="form-control" id="name" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email :</label>
+                            <input type="email" class="form-control" id="email" placeholder="Email :">
+                        </div>
+                        <div class="form-group">
+                            <label for="username">User Name :</label>
+                            <input type="email" class="form-control" id="username" placeholder="Username :">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password :</label>
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="cpassword">Confrim Password :</label>
+                            <input type="password" class="form-control" id="cpassword" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-block">Save</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
